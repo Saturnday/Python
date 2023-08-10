cbd2644fa1749fe04a9a04a6a56bf98ea6106540
Saturnday <mysaturnday@gmail.com>
2023-08-11 00:23:33 +0800

    anki

---
+++ anki.py
@@ -0,0 +1,30 @@
+#some excersises to learn syntax
+"""
+import sys
+def main():
+    if len(sys.argv)<2:
+        sys.exit("no arguments passed")
+    
+for arg in sys.argv[1:]
+    print("Hello, ", arg)
+main()
+
+def main():
+    try:
+        x=int(input("x=?"))
+        print("x=", x)
+    except:
+        print("the x is not int")
+main()
+
+def main():
+    square(3)
+
+def square(n):
+    for i in range(n):
+        for j in range(n):
+            print("#", end="")
+        print()
+main()
+"""
+assert
