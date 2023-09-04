4e38c0833bc4237ed460a52e1796a1cb4938bb0a
Saturnday <mysaturnday@gmail.com>
2023-09-04 14:18:11 +0800

    Read_Write_files

--- anki.py
+++ anki.py
@@ -26,5 +26,57 @@ def square(n):
             print("#", end="")
         print()
 main()
+assertx
+__init__.py
+
+def test_sayhello():
+    assert the_fucntion(argument)=="output"
+
+    for x in range(2):
+        print("")
+        print("x\n"*3, end="")
+
+x=input("what's x? ")        
+match x:
+    case "1":
+        print("")
+    case _:
+        print("all other ")
+
 """
-assert
+"""
+
+from calculator import square
+def main():
+    x=input(print("Name?"))    
+
+
+def test_square():
+    try:
+        assert square(3)==9
+    except:
+        print("square of 3 is not 9")
+
+if __name__ == "__main__":
+    main()
+import sys
+sys.argv
+
+name_of_dic={
+    "key": "value",
+}
+print(name_of_dic['Key'])
+
+
+name = input("what's your name?")
+print(f"Hello, {name}")
+
+"""
+
+#create a file to soter the list in
+
+
+name=input("what's your name?")
+file = open("names.txt", "w")
+file.write(name)
+file.close()
