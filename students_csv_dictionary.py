86a2b37f6353e10e7c75715bfef6cbf7b5b8298e
Saturnday <mysaturnday@gmail.com>
2023-09-14 13:38:09 +0800

    store_dic_csv_names

---
+++ students_csv_dictionary.py
@@ -0,0 +1,32 @@
+#-create a list to store values in it and make operations on them such as:
+# -read the file every line of it and put it in to the list
+# - use the 
+#  -sorted()
+#  -use list.append() to add to the list
+#-create a dictionary where i cant store value and name of the pairs from the csv file
+#-
+
+Students = []  # Create an empty list called 'Students' to store student data.
+
+with open("students.csv") as file:  # Open the file named "students.csv" for reading.
+    for line in file:  # Start iterating over each line in the file.
+
+        # Split the current line into two parts at the comma (',') and remove any leading/trailing whitespace.
+        Name, House = line.strip().split(",")
+
+        # Create an empty dictionary called 'Student' to store the student's data.
+        Student = {}
+
+        # Assign the 'Name' and 'House' values to the 'Student' dictionary.
+        Student['Name'] = Name
+        Student['House'] = House
+
+        # Append the 'Student' dictionary to the 'Students' list.
+        Students.append(Student)
+
+# Now that we've processed all lines in the file and populated the 'Students' list with dictionaries,
+# we'll iterate over each dictionary in the 'Students' list and print out the student's name and house.
+for Student in Students:
+    print(f"{Student['Name']} is in {Student['House']}")
+
+
