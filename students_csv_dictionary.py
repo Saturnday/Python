a6ad4b74a954c66b069832f5b31e5edeedbbcd57
Saturnday <mysaturnday@gmail.com>
2023-09-15 13:51:18 +0800

    lambda

--- students_csv_dictionary.py
+++ students_csv_dictionary.py
@@ -5,7 +5,7 @@
 #  -use list.append() to add to the list
 #-create a dictionary where i cant store value and name of the pairs from the csv file
 #-
-
+"""
 Students = []  # Create an empty list called 'Students' to store student data.
 
 with open("students.csv") as file:  # Open the file named "students.csv" for reading.
@@ -28,5 +28,18 @@ with open("students.csv") as file:  # Open the file named "students.csv" for rea
 # we'll iterate over each dictionary in the 'Students' list and print out the student's name and house.
 for Student in Students:
     print(f"{Student['Name']} is in {Student['House']}")
+"""
+#using lambda function (anonimus function)
+
+Students = []  
+with open("students.csv") as file:  
+    for line in file:  
+        Name, House = line.strip().split(",")
+        Student = {"name" : Name, 'house' : House}
+        Students.append(Student)
+
+for Student in sorted(Students, key=lambda student: student['name']):
+    print(f"{Student['name']} is in {Student['house']}")
+
 
 
